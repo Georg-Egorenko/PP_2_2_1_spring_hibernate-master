09818f39bfdb15dc41f6bbeadd600e249c563f7b
Georg Egorenko <test107782@gmail.com>
2025-08-20 11:53:12 +0300

    разделил слои

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImpl.java
@@ -0,0 +1,37 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CarDaoImpl implements CarDao {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public CarDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void addCar(Car car) {
+        sessionFactory.getCurrentSession().save(car);
+    }
+
+    @Override
+    public void updateCar(Car car) {
+        sessionFactory.getCurrentSession().update(car);
+    }
+
+
+    @Override
+    public List<Car> listCars() {
+        return sessionFactory.getCurrentSession()
+                .createQuery("from Car", Car.class)
+                .getResultList();
+    }
+}
